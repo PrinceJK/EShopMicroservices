9936d9b6321d218b7d20c2ec69dbecc469a76b81
John Kehinde <kehindejohn018@gmail.com>
2024-04-18 17:04:39 +0100

    Added pagination to /products

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -6,14 +6,6 @@ namespace Catalog.API.Products.GetProductById
     public record GetProductByIdQuery(Guid Id) : IQuery<Result<GetProductByIdResult>>;
     public record GetProductByIdResult(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
-    //public class GetCommandValidator : AbstractValidator<GetProductByIdQuery>
-    //{
-    //    public GetCommandValidator()
-    //    {
-    //        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
-    //    }
-    //}
-
     public class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, Result<GetProductByIdResult>>
     {
         public async Task<Result<GetProductByIdResult>> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,14 +1,18 @@
-﻿namespace Catalog.API.Products.GetProducts
-{
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
-    public record GetProductsResult(IEnumerable<Product> Products);
-    public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
-    {
-        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
-        {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+﻿using Marten.Pagination;
 
-            return new GetProductsResult(products);
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<Result<IEnumerable<GetProductsResult>>>;
+    public record GetProductsResult(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, Result<IEnumerable<GetProductsResult>>>
+    {
+        public async Task<Result<IEnumerable<GetProductsResult>>> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
+
+            var result = products.Adapt<IEnumerable<GetProductsResult>>();
+
+            return Result<IEnumerable<GetProductsResult>>.Success(result);
         }
     }
 }
