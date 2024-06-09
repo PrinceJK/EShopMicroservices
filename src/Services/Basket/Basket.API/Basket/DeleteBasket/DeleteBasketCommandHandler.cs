caf88d937fdeeae63fd5a7b1889cf022c1475eaf
John Kehinde <kehindejohn018@gmail.com>
2024-06-09 15:35:38 +0100

    added delete product

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<Result<bool>>;
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x=>x.UserName).NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, Result<bool>>
+{
+    public async Task<Result<bool>> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        //TODO: delete basket from database and cache
+        //session.Delete<Product>(command.Id);
+
+        return Result.Success(true);
+    }
+}
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,22 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketRequest(string UserName);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
-    public class DeleteBasketEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+
+            return Results.Ok(result);
+        })
+            .WithName("DeleteProduct")
+            .Produces<Result<bool>>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
     }
 }
