30befcf137e03f51afba6aa484eaf9ba67835654
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-07-18 01:34:08 +0100

    added abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Abstractiobs;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+}
+
+public interface IAggregate : IEntity
+{
+	IReadOnlyList<IDomainEvent> DomainEvents { get; }
+	IDomainEvent[] ClearDomainEvents();
+}
