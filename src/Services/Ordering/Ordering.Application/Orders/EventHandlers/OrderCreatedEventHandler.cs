a35a8a910376bd5fcd7aa018a5a8ba33206f2a81
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-22 18:18:10 +0100

    Added event handlers

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
+++
@@ -1,4 +0,0 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Application.Orders.EventHandlers;
-public record OrderCreatedEvent(Order order) : IDomainEvent;
--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
+++
@@ -1,4 +0,0 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Application.Orders.EventHandlers;
-public record OrderUpdatedEvent(Order order) : IDomainEvent;
