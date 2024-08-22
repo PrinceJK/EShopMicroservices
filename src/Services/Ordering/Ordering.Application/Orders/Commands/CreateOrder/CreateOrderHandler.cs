b05d696d331de09f99c183f49ad8ef3a28470acb
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-22 17:20:59 +0100

    added building blocks for create odrer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, Result<Guid>>
+{
+    public async Task<Result<Guid>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        //create command entiry
+        //save to database
+        //return result
+        throw new NotImplementedException();
+    }
+}
