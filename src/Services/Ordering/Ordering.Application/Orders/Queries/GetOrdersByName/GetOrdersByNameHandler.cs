94bc684ee62959e6becc5589604909ed3483a6d1
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-25 20:55:04 +0100

    implemented get Order by Customer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, Result<IEnumerable<OrderDto>>>
+{
+    public async Task<Result<IEnumerable<OrderDto>>> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.OrderName.Value.Contains(query.Name))
+                .OrderBy(o => o.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+        return Result<IEnumerable<OrderDto>>.Success(orders.ToOrderDtoList());
+    }
+}
