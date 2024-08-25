5985d76b656742869d9f56c05368f0e012813c9d
John Kehinde <kehindejohn018@gmail.com>
2024-08-26 00:50:58 +0100

    Feat/ordering.application layer cqrs implementation (#15)
    * Setup Order domain and database
    
    * setup sql server connection via docker
    
    * added db initializer extension
    
    * seeded data and implemented auto migrate
    
    * Added interceptor
    
    * cleaned code
    
    * Completed Order infrastructure
    
    * added domain dtos
    
    * added building blocks for create odrer
    
    * implemented create Order
    
    * removed unnessary usings
    
    * implemented update order
    
    * implemented delete order
    
    * created create and update event handlers class
    
    * Added event handlers
    
    * implemented get Order by Customer
    
    * implemented get orders
    
    * fixed build error

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+public class GetOrdersHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersQuery, Result<PaginatedResult<OrderDto>>>
+{
+    public async Task<Result<PaginatedResult<OrderDto>>> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await dbContext.Orders
+                       .Include(o => o.OrderItems)
+                       .OrderBy(o => o.OrderName.Value)
+                       .Skip(pageSize * pageIndex)
+                       .Take(pageSize)
+                       .ToListAsync(cancellationToken);
+
+        return Result<PaginatedResult<OrderDto>>.Success(
+            new PaginatedResult<OrderDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                orders.ToOrderDtoList()));
+    }
+}
