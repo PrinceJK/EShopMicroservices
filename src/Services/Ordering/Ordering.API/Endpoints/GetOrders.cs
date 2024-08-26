0368fa161d3f1afdedd76ceb5dfd674837e27e2d
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-26 19:13:13 +0100

    Implemented delete and Get Order endpoints

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
+
+public class GetOrders : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+
+            var response = result.Adapt<Result<PaginatedResult<OrderDto>>>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .Produces<Result<PaginatedResult<OrderDto>>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders")
+        .WithDescription("Get Orders");
+    }
+}
