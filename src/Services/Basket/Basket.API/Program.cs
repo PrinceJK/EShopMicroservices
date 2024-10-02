3e720b6c80634328ccaecd6d5ba5b990e73efcf3
John Kehinde <kehindejohn018@gmail.com>
2024-10-02 20:28:52 +0100

    Feat/asymc communication rabbit mass transit (#17)
    * Implemented
    
    * updated catalog appsettings and program class
    
    * Added basket checkout
    
    * completed ordering api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,12 @@
-using BuildingBlocks.Exceptions.Handler;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using BuildingBlocks.Messaging.MassTransit;
+using BuildingBlocks.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add services to the container
+// Add services to the container.
 
 //Application Services
 var assembly = typeof(Program).Assembly;
@@ -21,7 +22,7 @@ builder.Services.AddMediatR(config =>
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
-    opts.Schema.For<ShoppingCart>().Identity(x=>x.UserName);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
@@ -38,14 +39,19 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
 })
-    .ConfigurePrimaryHttpMessageHandler(() =>
+.ConfigurePrimaryHttpMessageHandler(() =>
 {
     var handler = new HttpClientHandler
     {
-        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
     };
+
     return handler;
 });
+
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 //Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
@@ -56,7 +62,7 @@ builder.Services.AddHealthChecks()
 
 var app = builder.Build();
 
-//Configure the HTTP request pipeline
+// Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 app.UseHealthChecks("/health",
--- src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -2,13 +2,13 @@
 
 public interface IEntity<T> : IEntity
 {
-	public T Id { get; set; }
+    public T Id { get; set; }
 }
 
 public interface IEntity
 {
-	public DateTime? CreatedAt { get; set; }
-	public string? CreatedBy { get; set; }
-	public DateTime? LastModified { get; set; }
-	public string? LastModifiedBy { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
