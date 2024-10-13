5fb8613a4ab2ee26d64134e8a8a0fb95d5f8332d
John Kehinde <kehindejohn018@gmail.com>
2024-10-13 22:05:11 +0100

    Feat/implementedapplicationclientapp (#19)
    * initialized client app
    
    * added basket and catalog model
    
    * implemented order model
    
    * Instantiated services interface
    
    * installed refit and configured it
    
    * Implemented ICatalogService
    
    * Implemented services
    
    * implemented index.cshtml
    
    * implemented cart view and logic
    
    * Implement product details page
    
    * Implemented check out
    
    * Manually created ids for seeded products
    
    * implemented confirmation page
    
    * implemented order list page
    
    * Implemented contact
    
    * completed client app
    
    * Complemented Aspnet mocroservice
    
    * stage

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+    Task<Result<IEnumerable<ProductModel>>> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+
+    [Get("/catalog-service/products/{id}")]
+    Task<Result<ProductModel>> GetProduct(Guid id);
+
+    [Get("/catalog-service/products/category/{category}")]
+    Task<Result<IEnumerable<ProductModel>>> GetProductsByCategory(string category);
+}
---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IOrderingService
+{
+    [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}\"")]
+    Task<Result<PaginatedResult<OrderModel>>> GetOrders(int? pageIndex = 1, int? pageSize = 10);
+
+    [Get("/ordering-service/orders/{orderName}")]
+    Task<Result<IEnumerable<OrderModel>>> GetOrdersByName(string orderName);
+
+    [Get("/ordering-service/orders/customer/{customerId}")]
+    Task<Result<IEnumerable<OrderModel>>> GetOrdersByCustomer(Guid customerId);
+}
