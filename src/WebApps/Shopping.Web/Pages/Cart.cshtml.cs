095e3af5e9f46aa44dbfb42a0a327ddabfe949e4
John Kehinde <kehindejohn018@gmail.com>
2024-10-13 20:22:52 +0100

    Implement product details page

--- src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
@@ -1,27 +1,26 @@
-namespace Shopping.Web.Pages
+namespace Shopping.Web.Pages;
+
+public class CartModel(IBasketService basketService, ILogger<CartModel> logger)
+    : PageModel
 {
-    public class CartModel(IBasketService basketService, ILogger<CartModel> logger)
-        : PageModel
+    public ShoppingCartModel Cart { get; set; } = new ShoppingCartModel();
+
+    public async Task<IActionResult> OnGetAsync()
     {
-        public ShoppingCartModel Cart { get; set; } = new ShoppingCartModel();
+        Cart = await basketService.LoadUserBasket();
 
-        public async Task<IActionResult> OnGetAsync()
-        {
-            Cart = await basketService.LoadUserBasket();
+        return Page();
+    }
 
-            return Page();
-        }
+    public async Task<IActionResult> OnPostRemoveToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Remove to cart button clicked");
+        Cart = await basketService.LoadUserBasket();
 
-        public async Task<IActionResult> OnPostRemoveToCartAsync(Guid productId)
-        {
-            logger.LogInformation("Remove to cart button clicked");
-            Cart = await basketService.LoadUserBasket();
+        Cart.Items.RemoveAll(x => x.ProductId == productId);
 
-            Cart.Items.RemoveAll(x => x.ProductId == productId);
+        await basketService.StoreBasket(new StoreBasketRequest(Cart));
 
-            await basketService.StoreBasket(new StoreBasketRequest(Cart));
-
-            return RedirectToPage();
-        }
+        return RedirectToPage();
     }
 }
